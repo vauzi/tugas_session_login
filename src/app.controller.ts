eacca0ca83b22438c8736b0699354c4c103573b5
vauzi <ismanvauzi998@gmail.com>
2022-12-14 12:06:30 +0700

    tugas pertama, session login

---
+++ src/app.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Res,
+  Render,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+import { LoginGuard } from './common/guards/login.guard';
+import { AuthenticatedGuard } from './common/guards/authenticated.guard';
+
+@Controller()
+export class AppController {
+  @Get('/')
+  @Render('login')
+  index() {
+    return;
+  }
+
+  @UseGuards(LoginGuard)
+  @Post('/login')
+  login(@Res() res: Response) {
+    res.redirect('/home');
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Get('/home')
+  @Render('home')
+  getHome(@Request() req) {
+    return { user: req.user };
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Get('/profile')
+  @Render('profile')
+  getProfile(@Request() req) {
+    return { user: req.user };
+  }
+
+  @Get('/logout')
+  logout(@Request() req, @Res() res: Response) {
+    req.session.destroy();
+    res.redirect('/');
+  }
+}
